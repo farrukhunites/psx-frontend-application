cb9c12ced17b3d0a3a58e16a30df1fc1f9d3b45c
farrukhunites <your-email@example.com>
2024-10-12 13:36:37 +0300

    Setting Up the Layout

--- src/Pages/Private/Dashboard/index.js
+++ src/Pages/Private/Dashboard/index.js
@@ -1,48 +1,7 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb, Row, Col, Card } from "antd";
-import { LineChart, BarChart } from "./Charts";
-import "./style.css";
-
-const { Header, Content, Footer } = Layout;
 
 const Dashboard = () => {
-  return (
-    <div className="dashboard">
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1">Dashboard</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: "0 50px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">
-            <Row gutter={16} className="graphs">
-              <Col span={8}>
-                <Card title="Other Data" bordered={false}>
-                  <p>Data Content</p>
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card title="Line Chart" bordered={false}>
-                  <LineChart />
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card title="Bar Chart" bordered={false}>
-                  <BarChart />
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </Content>
-      </Layout>
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
